1b6fc43e5196dd958d64c2bf318e90d2e352f235
VoyagerX21 <khakse2gaurav2003@gmail.com>
2025-09-01 18:13:20 +0530

    test1

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 function authUser(req, res, next){
     const token = req.cookies.auth;
     if (!token){
-        return res.render("login");
+        return res.render("login", {error: ""});
     }
     try{
         const decode = jwt.verify(token, process.env.SECRET_KEY);
@@ -11,7 +11,7 @@ function authUser(req, res, next){
         next();
     }
     catch (e) {
-        return res.render("login")
+        return res.render("login", {error: ""})
     }
 }
 
